6a80e1f7796f832852aecf911bf3bddf91b2126e
Allie Liu <allieliu@outlook.com>
2016-03-09 23:32:40 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var students = ["Mari", "Jenny", "Ahmed", "Singh"];
+var index= 0;
+for (var index= 0; index <5; index++) {
+  console.log(students[index]);
+}
+
+var students2 = ["Sean", "Ian", "Ilya", "Lisa", "Christina"]
+var i = 0;
+while (i < 5) {
+ console.log(students2[i])
+ i++;
+}
