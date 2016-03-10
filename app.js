316f0a5548255df1946150f1054463e077cf13ae
Allie Liu <allieliu@outlook.com>
2016-03-10 08:39:07 -0800

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+alert('It works!');
+
 var students = ["Mari", "Jenny", "Ahmed", "Singh"];
 var index= 0;
 for (var index= 0; index <5; index++) {
@@ -10,3 +12,23 @@ while (i < 5) {
  console.log(students2[i])
  i++;
 }
+
+for (var i=0; i<students.length; i++) {
+  console.log("A very cool student named Erik" + studentsErik[i] + " is also in my class.")
+}
+
+
+var St=["Ahmed", "George", "David", "Jenny", "Mari", "Matt"]
+var i=0
+while (i < St.length) {
+  console.log(St[i]);
+  i++;
+}
+
+var studentsMari = ["Singh", "Ahmed", "Evan", "Matt", "Lisa"]
+var indexy = 0;
+while (indexy < studentsMari.length) {
+  console.log(studentsMari[indexy])
+  indexy++;
+}
+Status API Training Shop Blog About Pricing
